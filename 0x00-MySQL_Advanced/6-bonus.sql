e70c8700243ff5f5713c999a2952cdd31ad35c1e
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-17 14:30:54 +0100

    Trigger

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,4 +1,4 @@
--- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+-- Creates a stored procedure AddBonus that adds a new correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
@@ -7,21 +7,18 @@ BEGIN
     DECLARE project_id INT DEFAULT 0;
 
     SELECT COUNT(id)
-    INTO project_count
-    FROM projects
-    WHERE name = project_name;
-
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
     IF project_count = 0 THEN
         INSERT INTO projects(name)
-            VALUES (project_name)
+            VALUES(project_name);
     END IF;
-
     SELECT id
         INTO project_id
         FROM projects
         WHERE name = project_name;
-
     INSERT INTO corrections(user_id, project_id, score)
         VALUES (user_id, project_id, score);
 END $$
-DELIMITER;
+DELIMITER ;
