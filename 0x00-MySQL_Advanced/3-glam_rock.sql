9e531955b42e5befb25712a00d5ab73186a340e7
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-17 12:47:17 +0100

    Enum

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (COALESCE(split, 2020) - formed)
-    AS lifespan FROM metal_bands
-                WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+-- Lists all bands with Glam as their main style, ranked by their longevity
+
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%';
