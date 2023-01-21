3bc25d78208bf71786f6ed6a04ea87fde8c8226c
Carmel Prosper SAGBO <labayifa@gmail.com>
2023-01-21 02:32:26 +0100

    Mongo DB NoSql

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     """
     insert into mongo_collection
     """
-    return mongo_collection.insert_one(kwargs).
+    return mongo_collection.insert_one(kwargs).inserted_id
